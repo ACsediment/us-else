d3f1802285ff7bdc7a39fb5cee791f02fcc8f6cc
ACsediment <ACsediment@users.noreply.github.com>
2015-05-31 02:46:52 +0800

    test

--- zhihu_visitor/Zhihu_Visitor.meta.js
+++ zhihu_visitor/Zhihu_Visitor.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name              Zhihu Visitor
-// @namespace         https://github.com/tiansh
+// @namespace         https://github.com/ACsediment
 // @description       知乎免登录查看回答全文
 // @name:zh           知乎免登录
 // @name:zh-CN        知乎免登录
 // @description:en    View zhihu.com answers without sign in
 // @description:zh-CN 知乎免登录查看回答全文并显示更多回答
 // @include           http://www.zhihu.com/*
-// @updateURL         http://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.meta.js
-// @downloadURL       http://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.user.js
+// @updateURL         https://raw.githubusercontent.com/ACsediment/us-else/master/zhihu_visitor/Zhihu_Visitor.meta.js
+// @downloadURL       https://raw.githubusercontent.com/ACsediment/us-else/master/zhihu_visitor/Zhihu_Visitor.user.js
 // @homepageURL       http://tiansh.github.io/us-else/zhihu_visitor/
 // @supportURL        https://github.com/tiansh/us-else/issues
 // @version           2.1
